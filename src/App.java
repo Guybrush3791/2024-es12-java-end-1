b26b306ff35e2932b60801fe6aab24311d9f3347
Giovanni Cappelletti <tech@methodwolf.com>
2024-06-26 14:44:35 +0200

    update

--- src/App.java
+++ src/App.java
@@ -77,5 +77,14 @@ public class App {
  * 
  * ---------------------------------------------------------------------------
  * 
+ * POMERIGGIO
  * 
+ * ---------------------------------------------------------------------------
+ * 
+ * ES5: Sistema di Votazione
+ * 
+ * Crea classi per Candidato e Elettore, dove ogni Elettore può votare per un
+ * Candidato. Implementa un meccanismo usando mappe per tenere traccia dei voti
+ * ricevuti da ogni candidato. Assicurati di gestire le eccezioni per casi come
+ * doppi voti o voti a candidati non esistenti.
  */
